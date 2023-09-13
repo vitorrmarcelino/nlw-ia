91df0d79b1f6d80cd0c4b91a71db9cd4029c4788
vitorrmarcelino <vitorrmarcelino09@gmail.com>
2023-09-13 16:53:23 -0300

    aula 2 finalizada

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,21 @@
 import { fastify } from "fastify";
 import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generationAICompletionRoute } from "./routes/generate-ai-completion";
+import {fastifyCors} from '@fastify/cors'
 
 
 const app = fastify();
 
+app.register(fastifyCors, {
+  origin: '*',
+})
+
 app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generationAICompletionRoute)
 
 app
   .listen({
